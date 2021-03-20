8089944918ab97908d7f39e05ed9748dc003bdbe
pasojucle <pasojucle@gmail.com>
2021-03-20 19:38:48 +0100

    List, édition et suppression des chaînes

---
+++ src/Form/ChannelType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Channel;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class ChannelType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Nom',
+                'row_attr' => ['class' => 'form-group'],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Channel::class,
+        ]);
+    }
+}
