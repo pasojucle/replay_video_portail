b2828e280dc2ad08a79eee5743a55c0509389a55
pasojucle <pasojucle@gmail.com>
2021-03-21 13:26:33 +0100

    webServices

---
+++ src/Controller/WebserviceController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Channel;
+use App\Entity\Program;
+use App\Repository\VideoRepository;
+use App\Repository\ChannelRepository;
+use App\Repository\ProgramRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class WebserviceController extends AbstractController
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/ws/videos", name="ws_video_list")
+     */
+    public function getVideos(
+        VideoRepository $videoRepository
+    ): JsonResponse
+    {
+     
+        return new JsonResponse([
+            'programs' => $videoRepository->findVideoToDownload(),
+        ]);
+    }
+
+    /**
+     * @Route("/ws/video/status", name="ws_video_status")
+     */
+    public function setVideoStatus(
+        Request $request,
+        VideoRepository $videoRepository
+    ): Response
+    {
+        $content = $request->getContent();
+
+        $data = json_decode($content, true);
+
+        if (null !== $data && array_key_exists('id', $data) && array_key_exists('status', $data)) {
+            $id = filter_var($data['id'], FILTER_VALIDATE_INT);
+            $status = filter_var($data['status'], FILTER_VALIDATE_INT);
+
+            if (false !== $id && false !==$status) {
+                $video = $videoRepository->find($id);
+                $video->setStatus($status);
+                $this->entityManager->flush();
+
+                return new Response('', 200);
+            }
+            
+        }
+        
+        return new Response('Format de donnée incorrect', 400);
+    }
+
+    /**
+     * @Route("/ws/program", name="ws_program")
+     */
+    public function setProgram(
+        Request $request,
+        ProgramRepository $programRepository
+    ): Response
+    {
+        $content = $request->getContent();
+
+        $data = json_decode($content, true);
+
+        if (null !== $data && array_key_exists('id', $data) && array_key_exists('id_raspberry', $data) && array_key_exists('title', $data)) {
+            $id = filter_var($data['id'], FILTER_VALIDATE_INT);
+            $idRaspberry = filter_var($data['id_raspberry'], FILTER_VALIDATE_INT);
+            $title = $data['title'];
+
+            if (false !==$idRaspberry) {
+                if (false !== $id ) {
+                    $program = $programRepository->find($id);
+                } else {
+                    $program = new Channel();
+                }
+                $program->setTitle($title)
+                    ->setIdRaspberry($idRaspberry);
+                $this->entityManager->persist($program);
+                $this->entityManager->flush();
+
+                return new Response('', 200);
+            }
+            
+        }
+        
+        return new Response('Format de donnée incorrect', 400);
+    }
+
+
+    /**
+     * @Route("/ws/channel", name="ws_channel")
+     */
+    public function setChannel(
+        Request $request,
+        ChannelRepository $channelRepository
+    ): Response
+    {
+        $content = $request->getContent();
+
+        $data = json_decode($content, true);
+
+        if (null !== $data && array_key_exists('id', $data) && array_key_exists('id_raspberry', $data) && array_key_exists('title', $data)) {
+            $id = filter_var($data['id'], FILTER_VALIDATE_INT);
+            $idRaspberry = filter_var($data['id_raspberry'], FILTER_VALIDATE_INT);
+            $title = $data['title'];
+
+            if (false !==$idRaspberry) {
+                if (false !== $id ) {
+                    $channel = $channelRepository->find($id);
+                } else {
+                    $channel = new Program();
+                }
+                $channel->setTitle($title)
+                    ->setIdRaspberry($idRaspberry);
+                $this->entityManager->persist($channel);
+                $this->entityManager->flush();
+
+                return new Response('', 200);
+            }
+            
+        }
+        
+        return new Response('Format de donnée incorrect', 400);
+    }
+}
